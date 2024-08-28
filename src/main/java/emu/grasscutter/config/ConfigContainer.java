4e41cb9dce469825c9375b63a92ecab233b9a894
WatchAndyTW <andyediteroffice@gmail.com>
2024-08-28 11:29:27 +0800

    Update default config

--- src/main/java/emu/grasscutter/config/ConfigContainer.java
+++ src/main/java/emu/grasscutter/config/ConfigContainer.java
@@ -175,7 +175,7 @@ public class ConfigContainer {
         /* Enabling this will generate a unique packet encryption key for each player. */
         public boolean useUniquePacketKey = true;
 
-        public boolean useXorEncryption = false;
+        public boolean useXorEncryption = true;
 
         /* Entities within a certain range will be loaded for the player */
         public int loadEntitiesForPlayerRange = 300;
